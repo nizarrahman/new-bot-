1ba83ff4c9fbf550091733eb634402dea1f8ef38
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-12-27 00:11:11 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -13,7 +13,7 @@ global.rep = {
             participant : '0@s.whatsapp.net'},
             message: { "liveLocationMessage": { "title": gcname,"h": bottime, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg')}}
            }
-conn.sendButtonVid(m.chat, giflogo, `𝐃𝐎𝐍𝐀𝐒𝐈`, `⮕ 𝐆𝗼𝐩𝐚𝐲 = [${pgopay}]\n⮕ 𝐃𝐚𝐧𝐚 = [${pdana}]\n\n𝐍𝗼𝐭𝐞 : 𝐉𝐢𝐤𝐚 𝐒𝐮𝐝𝐚𝐡 𝐃𝗼𝐧𝐚𝐬𝐢 𝐒𝐢𝐥𝐚𝐡𝐤𝐚𝐧 𝐊𝐢𝐫𝗺\n𝐁𝐮𝐤𝐭𝐢 𝐊𝐞 𝐎𝐰𝐧𝐞𝐫!!\n𝐀𝐭𝐚𝐮 𝐊𝐥𝐢𝐤 𝐃𝐢 𝐁𝐚𝐰𝐚𝐡
+conn.sendButtonVid(m.chat, giflogo, `𝐃𝐎𝐍𝐀𝐒𝐈`, `⮕ Pulsa = [${ppulsa}]\n\n𝐍𝗼𝐭𝐞 : 𝐉𝐢𝐤𝐚 𝐒𝐮𝐝𝐚𝐡 𝐃𝗼𝐧𝐚𝐬𝐢 𝐒𝐢𝐥𝐚𝐡𝐤𝐚𝐧 𝐊𝐢𝐫𝗺\n𝐁𝐮𝐤𝐭𝐢 𝐊𝐞 𝐎𝐰𝐧𝐞𝐫!!\n𝐀𝐭𝐚𝐮 𝐊𝐥𝐢𝐤 𝐃𝐢 𝐁𝐚𝐰𝐚𝐡
 `, 'Owner', '.owner', fkontak,)
 }
 handler.tags = ['donasi', 'donate']
